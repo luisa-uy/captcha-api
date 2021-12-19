314a7b159dc2df2d21dc0f847194c86ba4eb5b65
Gabriel Núñez <gnuy@pm.me>
2021-12-19 02:27:05 -0300

    No entiendo un choto de como hacer funcionar la modularidad, queda para después. I'm very sowi.

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-from captchaluisa.models import Bloque, Intento
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+class Bloque(db.Model):
+	"""Modelo para bloques de texto"""
+	__tablename__ = 'bloque'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	path_imagen = db.Column(db.Text)
+	imagen = db.Column(db.Text)
+	texto = db.Column(db.Text)
+	intentos = db.relationship('Intento')
+
+	def __repr__(self):
+		return '<Bloque {}>'.format(self.path_imagen)
+
+class Intento(db.Model):
+	"""Modelo para intento de lectura"""
+	__tablename__ = 'intento'
+	fecha_hora = db.Column(db.DateTime, primary_key=True)
+	texto = db.Column(db.Text)
+	bloque_id = db.Column(db.Integer, db.ForeignKey('bloque.id'), nullable=False)
+
+	def __repr__(self):
+		return '<Intento {}>'.format(self.texto)
+
+db.create_all()
 
 @app.route("/")
 def hello():
