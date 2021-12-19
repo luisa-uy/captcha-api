b9e53b1d57288871c3510a7288e7897fdcbe90dd
Gabriel Núñez <gnuy@pm.me>
2021-12-19 17:38:12 -0300

    GET /api/bloque para obtener todos los bloques

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, jsonify
+from flask_marshmallow import Marshmallow
 from config import Config
 from datetime import datetime
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Bloque(db.Model):
-	"""Modelo para bloques de texto"""
+	# Modelo para bloques de texto
 	__tablename__ = 'bloque'
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	path_imagen = db.Column(db.Text)
@@ -17,34 +19,57 @@ class Bloque(db.Model):
 	texto = db.Column(db.Text)
 	intentos = db.relationship('Intento')
 
-	def __repr__(self):
-		return '<Bloque {}>'.format(self.path_imagen)
-
 class Intento(db.Model):
-	"""Modelo para intento de lectura"""
+	# Modelo para intento de lectura
 	__tablename__ = 'intento'
-	fecha_hora = db.Column(db.DateTime, 
-						primary_key=True,
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	fecha_hora = db.Column(db.DateTime,
 						default=datetime.utcnow, 
 						onupdate=datetime.utcnow)
 	texto = db.Column(db.Text)
 	bloque_id = db.Column(db.Integer, db.ForeignKey('bloque.id'), nullable=False)
 
-	def __repr__(self):
-		return '<Intento {}>'.format(self.texto)
+class BloqueSchema(ma.SQLAlchemyAutoSchema):
+	class Meta:
+		# fields = ("id", "path_imagen", "imagen", "texto", "_links")
+		model = Bloque
+		include_fk = True
+		load_instance = True
+
+		# id = ma.auto_field()
+		# path_imagen = ma.auto_field()
+		# imagen = ma.auto_field()
+		# texto = ma.auto_field()
+		# intentos = ma.auto_field()
+
+	# _links = ma.Hyperlinks({
+	# 	'self': ma.URLFor('bloque', values=dict(id='<id>')),
+	# 	'collection': ma.URLFor('bloques'),
+	# })
+		
+class IntentoSchema(ma.SQLAlchemyAutoSchema):
+	class Meta:
+		model = Intento
+		include_fk = True
+		load_instance = True
+		
 
 db.create_all()
+bloque_schema = BloqueSchema()
+bloques_schema = BloqueSchema(many=True)
+intento_schema = IntentoSchema()
+intentos_schema = IntentoSchema(many=True)
+
+db.session.commit()
 
 @app.route("/")
 def hello():
 	return "<h1 style='color:blue'>Hello There!</h1>"
 
-@app.route("/bloque", methods=['GET'])
+@app.route("/api/bloque/")
 def get_bloques():
-	bloques = Bloque.query.order_by(Bloque.id).all()
-	for bloque in bloques:
-		print(f'{bloque.id} {bloque.path_imagen} {bloque.texto}')
-	return jsonify(bloques)
+	all_bloques = Bloque.query.all()	
+	return (jsonify({ "data" :  bloques_schema.dump(all_bloques) } ), 200)
 
 @app.route("/captcha")
 def obtener_captcha():
