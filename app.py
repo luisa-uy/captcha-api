ce94ba4bb4df45028add9b31fb1ea5dd0a868a2a
Gabriel Núñez <gnuy@pm.me>
2021-12-21 03:54:58 -0300

    Creada la clase Captcha contiene la info de si se resolvió correctamente, como también un token y la fecha Se relaciona con N Bloques, dado que un captcha tiene que estar integrado por distintos bloques. El endpoint GET /api/captcha por el momento nos devuelve 4 bloques random, indiferentemente si están resueltos o no. Habría que agregarle lógica al random para que nos devuelva al menos 1 sin resolver y al menos 1 resuelto. También hay que crear el POST que recibe la información desde el frontend, y guardar en base un Captcha nuevo cuando se llama este GET.

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask_marshmallow import Marshmallow
 from config import Config
 from datetime import datetime
 from flask_restful import Api, Resource
+import random
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+captcha_size = 4 
 
 class Bloque(db.Model):
 	# Modelo para bloques de texto
@@ -18,6 +20,7 @@ class Bloque(db.Model):
 	imagen = db.Column(db.Text)
 	texto = db.Column(db.Text)
 	intentos = db.relationship('Intento')
+	captchas = db.relationship("Captcha", secondary='bloque_captcha', back_populates='bloques')
 
 class Intento(db.Model):
 	# Modelo para intento de lectura
@@ -29,36 +32,46 @@ class Intento(db.Model):
 	texto = db.Column(db.Text)
 	bloque_id = db.Column(db.Integer, db.ForeignKey('bloque.id'), nullable=False)
 
+class Captcha(db.Model):
+	# Modelo para Captcha
+	__tablename__ = 'captcha'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	fecha_hora = db.Column(db.DateTime, default=datetime.utcnow)
+	status = db.Column(db.Boolean, default=False)
+	token = db.Column(db.String)
+	bloques = db.relationship("Bloque", secondary="bloque_captcha", back_populates='captchas')
+
+class BloqueCaptcha(db.Model):
+	# ManyToMany entre Bloque y Captcha
+	__tablename__ = 'bloque_captcha'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	bloques = db.Column(db.Integer, db.ForeignKey('bloque.id'))
+	captchas = db.Column(db.Integer, db.ForeignKey('captcha.id'))
+
+
 class BloqueSchema(ma.SQLAlchemyAutoSchema):
 	class Meta:
-		# fields = ("id", "path_imagen", "imagen", "texto", "_links")
 		model = Bloque
 		include_fk = True
 		load_instance = True
-
-		# id = ma.auto_field()
-		# path_imagen = ma.auto_field()
-		# imagen = ma.auto_field()
-		# texto = ma.auto_field()
-		# intentos = ma.auto_field()
-
-	# _links = ma.Hyperlinks({
-	# 	'self': ma.URLFor('bloque', values=dict(id='<id>')),
-	# 	'collection': ma.URLFor('bloques'),
-	# })
 		
 class IntentoSchema(ma.SQLAlchemyAutoSchema):
 	class Meta:
 		model = Intento
 		include_fk = True
 		load_instance = True
-		
+
+class CaptchaSchema(ma.SQLAlchemyAutoSchema):
+	class Meta:
+		model = Captcha
+		load_instance = True
 
 db.create_all()
 bloque_schema = BloqueSchema()
 bloques_schema = BloqueSchema(many=True)
 intento_schema = IntentoSchema()
 intentos_schema = IntentoSchema(many=True)
+captcha_schema = CaptchaSchema()
 
 db.session.commit()
 
@@ -69,10 +82,24 @@ def hello():
 @app.route("/api/bloque/")
 def get_bloques():
 	all_bloques = Bloque.query.all()	
-	return (jsonify({ "data" :  bloques_schema.dump(all_bloques) } ), 200)
+	return (jsonify(bloques_schema.dump(all_bloques)), 200)
 
-@app.route("/captcha")
+
+@app.route("/api/intento/")
+def get_intentos():
+	all_intentos = Intento.query.all()
+	return (jsonify(intentos_schema.dump(all_intentos)), 200)
+
+
+@app.route("/api/captcha/")
 def obtener_captcha():
+
+	res = []
+	size=db.session.query(Bloque).count()
+	for i in range(captcha_size):
+		res.append(db.session.query(Bloque)[random.randrange(0, size)])
+	return jsonify(bloques_schema.dump(res), 200)
+		
 	try:
 		return "OK"
 	except Exception as ex:
